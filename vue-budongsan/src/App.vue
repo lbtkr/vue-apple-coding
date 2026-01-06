f7ea427efee431d5efd43d20f18a650511101e20
강희주 <k.lbtkr@gmail.com>
2026-01-06 15:16:03 +0900

    2강. HTML에 데이터 꽂아넣는 Vue 데이터바인딩 문법

--- vue-budongsan/src/App.vue
+++ vue-budongsan/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-
+    <img src="./assets/vue.svg" alt="">
+    <div v-for="val in products" :key="val">
+        <h4>{{ val }}</h4>
+        <p>80 만원</p>
     </div>
 </template>
 
 <script setup>
+    const products = ['역삼동 원룸', '신림동 원룸', '강남동 원룸'];
 
 </script>
 
